34d4bd36d27bae319fcc72aecb9810166f028c58
Stefan Einarsson <stefan@retinarisk.com>
2025-05-24 13:13:30 +0200

    Add dependencies to executable module in build configuration

--- zig/heightmap/build.zig
+++ zig/heightmap/build.zig
@@ -65,6 +65,7 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
+    deps.addAllTo(exe);
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
